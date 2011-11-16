410f402a612eac85bc86966fe825577c79540949
Greg Nicholas <greg.nicholas@gmail.com>
2011-11-16 18:07:28 -0500

    skeleton of models and tests written

--- places/models.py
+++ places/models.py
@@ -1,3 +1,88 @@
 from django.db import models
 
-# Create your models here.
+# TODO: largely a placeholder, flesh out more later
+class Neighborhood(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.name
+
+class Location(models.Model):
+	'''
+	Handles specific information about where a physical place is located.
+	
+	A location must include either a longitude/latitude pair or street address.
+	'''
+	# 2-char country code (see http://en.wikipedia.org/wiki/ISO_3166-1)
+	country = models.CharField(max_length=2,blank=True)
+
+	# if US state, this field should either contain 2 letters or be blank
+	state = models.CharField(max_length=30,blank=True)
+
+	town = models.CharField(max_length=60,blank=True)
+	neighborhood = models.ForeignKey(Neighborhood,blank=True,null=True)
+	
+	postcode = models.CharField(max_length=10,blank=True)
+	address = models.TextField('street address',blank=True)
+
+	# should always be between -90,90
+	latitude = models.DecimalField(max_digits=9,decimal_places=6,blank=True,null=True)	
+	# should always be between -180,180
+	longitude = models.DecimalField(max_digits=9,decimal_places=6,blank=True,null=True)	
+
+	def _normalize_address(self):
+		'''
+		Replaces the current contents of the address field with a normalized
+		version of it (e.g. '201 S. Bouquet Street' -> '201 S Bouquet St').
+
+		Currently uses Google Geocoding API's formatted_address field in 
+		development, though this should change.
+		'''
+		# TODO: revisit address normalization api
+		pass
+	
+	def _complete_missing_fields(self):
+		'''
+		Refers to an authoritative source to complete missing fields. 
+		
+		Currently uses Google Geocoding API, but will hopefully move to 
+		Factual Resolve API.
+		'''
+		# TODO: revisit missing location info api
+		pass
+
+	def __unicode__(self):
+		return u'%s (%.3f,%.3f)' % (self.address,self.latitude,self.longitude)
+
+class Place(models.Model):
+	'''
+	Handles information about places.
+	'''
+	dtcreated = models.DateTimeField('dt created',auto_now_add=True)
+	
+	name = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+	url = models.URLField(max_length=400,blank=True)
+	location = models.ForeignKey(Location,blank=True,null=True)
+
+	def __unicode__(self):
+		return self.name
+
+class PlaceMeta(models.Model):
+	'''
+	Handles meta information (tags, external API ids, etc.) for a Place.
+	'''
+	place = models.ForeignKey(Place)
+	meta_key = models.CharField(max_length=100)
+	# blank values allowed (boolean meta attributes)
+	meta_value = models.TextField(blank=True)
+
+class LocationMeta(models.Model):
+	'''
+	Handles meta information (tags, external API ids, etc.) for a Place.
+	'''
+	location = models.ForeignKey(Location)
+	meta_key = models.CharField(max_length=100)
+	# blank values allowed (boolean meta attributes)
+	meta_value = models.TextField(blank=True)
--- places/tests.py
+++ places/tests.py
@@ -1,16 +1,41 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+class LocationModelTest(TestCase):
+    def test_two_letter_us_state(self):
+        """
+        Tests that all US states have 2-letter entries.
+        """
+        # create a location and ensure US state with 1 or more than 3 letters fails. Also ensure that other countries have no such limits.
+        self.fail()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_geocode_bounds(self):
+    	'''
+    	Ensures latitude and longitude are within accepted ranges.
+    	'''
+    	# test latitude is in [-90,90] and longitude is in [-180,180]
+    	self.fail()
+
+    def test_minimum_information(self):
+    	'''
+    	Ensures a Location has a minimum amount of information before
+    	it can be saved.
+    	'''
+    	# try to save an address without both long/lat and street address.
+    	# also ensure providing either one or the other is ok
+    	self.fail()
+
+    def test_address_normalization(self):
+    	'''
+    	Ensures all street addresses saved in the DB are normalized.
+    	'''
+    	# Try to save a non-normalized address and see if it gets normalized
+    	# include interesting cases like apartment numbers
+    	self.fail()
+
+    def test_missing_field_completion(self):
+    	'''
+    	Various tests to ensure missing fields are filled if enough
+    	information is given.
+    	'''
+    	# do a bunch of tests here with some fairly easy pieces of information
+    	self.fail()
