f73b0b53b92113cfad2803ee106f6c0aa311ad51
Greg Nicholas <greg.nicholas@gmail.com>
2011-11-22 17:26:30 -0500

    initial offers app/model

---
+++ offers/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from onlyinpgh.identity.models import Organization
+from onlyinpgh.tagging.models import Tag
+
+class Offer(models.Model):
+    description = models.TextField()
+    point_value = models.PositiveIntegerField()
+    sponsor = models.ForeignKey(Organization)
+
+    tags = models.ManyToManyField(Tag,blank=True,null=True)
