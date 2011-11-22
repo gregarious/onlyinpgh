0bfb0502c17d4d3f3589785d0c18abeefa0d46c2
Greg Nicholas <greg.nicholas@gmail.com>
2011-11-22 17:12:10 -0500

    additions/bugfix for identity

--- identity/models.py
+++ identity/models.py
@@ -3,8 +3,13 @@ from django.contrib.auth.models import User
 
 class Identity(models.Model):
     # account is not required to have an identity on the site
-    account = models.ManyToManyField(User,unique=True,null=True,blank=True)
+    account = models.ManyToManyField(User,null=True,blank=True)
     avatar = models.ImageField(upload_to='avatars')
+
+    display_name = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.display_name
 
 class Organization(models.Model):
     identity = models.ForeignKey(Identity,unique=True,blank=True,null=True)
