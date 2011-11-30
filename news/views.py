fe0035b9209b8da6fc6233c9154d565bc53e3c28
Greg Nicholas <greg.nicholas@gmail.com>
2011-11-30 17:40:08 -0500

    broadcast ready with basic demo templates & views

--- news/views.py
+++ news/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from news.models import Article
+
+def demo_news(request):
+    variables = { 'articles': Article.objects.all() }
+    return render_to_response('broadcast/news.html',variables)
--- urls.py
+++ urls.py
@@ -3,6 +3,12 @@ from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from onlyinpgh.places import views as places_views
+from onlyinpgh.events import views as events_views
+from onlyinpgh.offers import views as offers_views
+from onlyinpgh.news import views as news_views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,6 +18,10 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url('^$',           direct_to_template,     {'template':'base.html'}),
-    url(r'^admin/',     include(admin.site.urls)),
+    url(r'^$',      direct_to_template,     {'template':'base.html'}),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ajax/places$', places_views.demo_establishments),
+    url(r'^ajax/events$', events_views.demo_events),
+    url(r'^ajax/offers$', offers_views.demo_offers),
+    url(r'^ajax/news$', news_views.demo_news),
 )
