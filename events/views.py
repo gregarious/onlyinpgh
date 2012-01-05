c4ac1f5c9d9481098c1f11acbc2a6e61d4660d90
Greg Nicholas <greg.nicholas@gmail.com>
2012-01-05 00:28:02 -0500

    added event view filter to eliminate dummy event

--- events/views.py
+++ events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from onlyinpgh.events.models import Event
 
 def demo_events(request):
-    variables = { 'events': Event.objects.all() }
+    variables = { 'events': Event.objects.filter(invisible=False) }
     return render_to_response('feeds/events_feed.html',variables)
 
 
@@ -10,10 +10,10 @@ def demo_events(request):
 # Duplicating functions for the sake of speed
 
 def demo_events_page(request):
-    variables = { 'events': Event.objects.all() }
+    variables = { 'events': Event.objects.filter(invisible=False) }
     return render_to_response('pages/events_page.html',variables)
 
 
 def demo_events_single(request):
-    variables = { 'events': Event.objects.all() }
+    variables = { 'events': Event.objects.filter(invisible=False) }
     return render_to_response('single/event_single.html',variables)
