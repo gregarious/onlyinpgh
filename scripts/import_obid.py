a2ce195a8666152f15e0056ea9d96e565cbf9dc6
Greg Nicholas <greg.nicholas@gmail.com>
2012-01-17 16:11:13 -0500

    new OBID file and script changes - untested

--- scripts/import_obid.py
+++ scripts/import_obid.py
@@ -1,8 +1,21 @@
-import csv, time, os
+import csv, time, os, re
 
 from onlyinpgh.places.models import Location, Place, Meta as PlaceMeta
 from onlyinpgh.outsourcing.places import resolve_location
 from onlyinpgh.identity.models import Organization
+from onlyinpgh.outsourcing.models import ExternalPlaceSource
+from onlyinpgh.outsourcing.apitools.facebook import oip_client as fb_client
+
+def save_facebook_ref(fb_id,place):
+    assert fb_id and place
+    if not re.match('\d+$',fb_id):  # if there's some alphanumeric chars in the username
+        try:
+            response = fb_client.graph_api_objects_request(fb_id)
+            fb_id = response['id']
+        except Exception as e:
+            print 'Error retreiving ID %s: %s' % (str(fb_id),str(e))
+    
+    ExternalPlaceSource.objects.create(service='fb',uid=fb_id,place=place)
 
 def run():
     in_filename = os.path.join(os.path.dirname(__file__),'obid.csv')
@@ -13,6 +26,7 @@ def run():
     PlaceMeta.objects.all().delete()
     Place.objects.all().delete()
     Organization.objects.all().delete()
+    ExternalPlaceSource.objects.all().delete()
 
     with open(err_filename,'w') as err_f:
         writer = csv.writer(err_f)
@@ -24,20 +38,10 @@ def run():
             for i,row in enumerate(reader):
                 if i % 50 == 1:
                     print 'row %d of %d completed' % (i,num_rows)
-                try:
-                    org_str, place_str, loc_str = map(unicode,row[1:4])
-                except UnicodeDecodeError:
-                    writer.writerow(row)
-                    continue
-
-                if len(row) > 4:
-                    phone = row[4]
-                else:
-                    phone = ''
-                if len(row) > 5:
-                    url = row[5]
-                else:
-                    url = ''
+                # ensure each row is at least 6 fields long
+                if len(row) < 6:
+                    row.extend(['']*(6-len(row)))
+                org_str, place_str, loc_str, phone, url, _, fb_id = [x.strip() for x in row[:6]]
 
                 if org_str == '':
                     if place_str == '':
@@ -46,7 +50,6 @@ def run():
                     else:
                         org_str = place_str
 
-                time.sleep(.1)
                 location = resolve_location(Location(address=loc_str,postcode='15213'))
                 if not location:
                     writer.writerow(row)
@@ -62,14 +65,21 @@ def run():
                     location.save()
 
                 org,created = Organization.objects.get_or_create(name=org_str)
-                if not created:
+                if created:
+                    if org_str == place_str and url:
+                        org.url = url
+                        org.save()
+                else:
                     print '%s already exists' % org
                 
                 place,created = Place.objects.get_or_create(name=place_str,location=location,owner=org)
                 if not created:
                     raise Exception('%s already exists?' % str(place))
-                print 'inserted',unicode(place)
+                print 'inserted',place
 
                 if url:
                     PlaceMeta.objects.create(place=place,meta_key='url',meta_value=url)
+                if phone:
                     PlaceMeta.objects.create(place=place,meta_key='phone',meta_value=phone)
+                if fb_id:
+                    save_facebook_ref(fb_id,place)
