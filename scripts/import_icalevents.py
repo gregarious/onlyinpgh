f5d901a6a1768ed626bfdb9bca0311a5fdfa3287
Greg Nicholas <greg.nicholas@gmail.com>
2012-01-27 00:12:14 -0500

    adjusted ical pulling script to not report ignore existing events

--- scripts/import_icalevents.py
+++ scripts/import_icalevents.py
@@ -11,8 +11,10 @@ def import_all():
     
     importlog.info('Reading %d feeds for Event importing' % len(feeds))
     
+
     for feed in feeds:
         import_count = 0
+        exists_count = 0
         importlog.info('Processing new events from feed "%s"' % (unicode(feed)))
         importer = FeedImporter(feed)
         for report in importer.import_new(start_filter=datetime(2012,1,1)):
@@ -21,18 +23,21 @@ def import_all():
                 continue
             record = report.vevent_record
             if record.event:
-                importlog.info(u'UID %s: Imported new event, id=%s' % (record.uid,record.event.id))
-                # increment the counter for every new event added
                 if len([1 for n in report.notices 
                         if isinstance(n,EventImportReport.RecordExists)]) == 0:
+                    importlog.info(u'UID %s: Imported new event, id=%s' % (record.uid,record.event.id))
+                    # increment the counter for every new event added
                     import_count += 1
             else:
                 importlog.warning(u'UID %s: Event import failed due to a problem with the feed entry.' % report.uid)
             for notice in report.notices:
-                importlog.info(u'UID %s: Report status notice: %s' % (record.uid,notice))
-            
-        importlog.info('%d new Events imported from feed "%s"' % (import_count,unicode(feed)))
+                if isinstance(notice,EventImportReport.RecordExists):
+                    exists_count += 1
+                else:  
+                    importlog.info(u'UID %s: Report status notice: %s' % (record.uid,notice))
 
+        importlog.info('%d new Events imported from feed "%s" (%d existing skipped)' % (import_count,unicode(feed),exists_count))
+        
 def run():
     try:
         # TODO: have import_all return VEvents, not Events
